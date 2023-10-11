3f776b009f9c4dcb664dc6187f8213bc2f312481
LenaPotatosova <potatosovae@gmail.com>
2023-10-11 13:35:16 +1300

    ghappy tests

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,17 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import tudelft.ghappy.GHappy;
+
 public class GHappyTest {
+    @ParameterizedTest(name = "string={0}, result={1}")
+    @CsvSource({ "zxggxtfd, true", "ggxcv, true", "dfsgg, true", "gxdr, false", "dfvg, false", "fgdg, false" })
+    public void testInput(String input, Boolean expectedResult) {
+        GHappy g = new GHappy();
+        boolean result = g.gHappy(input);
+        Assertions.assertEquals(expectedResult, result);
+    }
 }
