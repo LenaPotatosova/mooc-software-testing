90a4e667f8c7b6e7b6a62359e1077c4b4c59d00c
LenaPotatosova <potatosovae@gmail.com>
2023-10-11 18:26:51 +1300

    caesar

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,17 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import tudelft.caesarshift.CaesarShiftCipher;
+
 public class CaesarShiftCipherTest {
+    @ParameterizedTest(name = "message={0}, shift={1}, result={2}")
+    @CsvSource({ "abc, 3, def", "z, 1, a", "a b, 1, b c", "!, 3, invalid", "a, -1, z"})
+    public void testInput(String message, int shift, String expectedResult) {
+        CaesarShiftCipher cipher = new CaesarShiftCipher();
+        String result = cipher.CaesarShiftCipher(message, shift);
+        Assertions.assertEquals(expectedResult, result);
+    }
 
 }
